efbdde32c69a41aeba437530a7b5ed3ab49955a3
Xander-UXDG <xander.rgrant@gmail.com>
2025-02-20 21:04:35 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+        document.querySelector(".draw-button").addEventListener("click", function() {
+            window.location.href = "gallery.html";
+        });
+    });
