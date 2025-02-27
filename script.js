3c20b84642915d86dde3c1b805c6f561f5508894
cknowles320 <cknowl21@student.scad.edu>
2025-02-27 18:47:18 -0500

    faster

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
         targets: ".vertical-carousel",
         translateY: ["0%", "-100%"],
         easing: "linear",
-        duration: 5000,
+        duration: 2000,
         loop: true
     });
 });
